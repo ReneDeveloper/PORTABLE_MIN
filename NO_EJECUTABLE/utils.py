3ffad4cbec5b2e6c7fab6359e48cf1e880156c85
rsilcas <rene.silva.castillo@gmail.com>
2026-05-29 12:12:40 -0400

    UPDATE_20260529

---
+++ NO_EJECUTABLE/utils.py
@@ -0,0 +1,35 @@
+utils.py
+
+https://github.com/MachinaCore/CygwinPortable/releases
+
+
+DATOS DE BBDD:
+https://www.linkedin.com/pulse/how-determine-last-modified-date-tables-oracle-ahmet-duru%C3%B6z/
+
+
+
+#tesseract
+#https://www.youtube.com/watch?v=OutxRdkNOK4
+#https://pypi.org/project/pytesseract/
+#https://tesseract-ocr.github.io/tessdoc/Installation.html
+#https://github.com/UB-Mannheim/tesseract/wiki
+#https://github.com/UB-Mannheim/tesseract/releases/download/v5.4.0.20240606/tesseract-ocr-w64-setup-5.4.0.20240606.exe
+
+
+
+TEMPLATES_WEB_GIT
+https://github.com/SupunKavinda/jekyll-theme-leaf
+https://github.com/tocttou/hacker-blog
+https://github.com/longpdo/neumorphism
+https://github.com/slemire/slemire.github.io
+
+
+#DIGERIDOS COMPLETO:
+https://www.youtube.com/watch?v=OutxRdkNOK4
+
+
+#OBS PORTABLE
+
+https://github.com/obsproject/obs-studio/releases
+https://github.com/obsproject/obs-studio/releases
+https://github.com/obsproject/obs-studio/releases/download/30.2.2/OBS-Studio-30.2.2-Windows.zip
